b5131156f51e2435d55794fe3c7e6b218c4859fd
flamecata <flamecata@yahoo.com>
2016-05-26 16:40:25 +0300

    Added new features

--- Maximum Entropy/MaximumEntropy.py
+++ Maximum Entropy/MaximumEntropy.py
@@ -7,6 +7,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 
+
 def nltk_test():
     doc = 'John lives in London and works at IBM. Lucy works at Intel in Berlin. ' \
           'Catalin works at Integrisoft in Bucharest'
@@ -48,33 +49,69 @@ def generate_features(sentence):
     features = []
 
     for i, (word, tag_tuple) in enumerate(sentence):
-        word_features = []
-        word_features.append(word)
-        word_features.append(tag_tuple[0])
-        word_features.append(tag_tuple[2])
+        word_features = [word, tag_tuple[0]]
 
+        previous2PosTag = "None"
         previousPosTag = "None"
-        previousWord = "None"
-        nextPosTag = "None"
-        nextWord = "None"
 
+        previous2Word = "None"
+        previousWord = "None"
+
+        nextPosTag = "None"
+        next2PosTag = "None"
+
+        nextWord = "None"
+        next2Word = "None"
+
+        phraseStart = i == 0
+        phraseEnd = i == len(sentence) - 1
+        if i == 0 and len(sentence) > 2:
+            nextPosTag = sentence[i + 1][1][0]
+            next2PosTag = sentence[i + 2][1][0]
+
+            nextWord = sentence[i + 1][0]
+            next2Word = sentence[i + 2][0]
         if i == 0 and len(sentence) > 1:
             nextPosTag = sentence[i + 1][1][0]
             nextWord = sentence[i + 1][0]
+        elif i == len(sentence) - 2:
+            previousPosTag = sentence[i - 1][1][0]
+            previous2PosTag = sentence[i - 2][1][0]
+
+            previousWord = sentence[i - 1][0]
+            previous2Word = sentence[i - 2][0]
         elif i == len(sentence) - 1:
             previousPosTag = sentence[i - 1][1][0]
             previousWord = sentence[i - 1][0]
         else:
             nextPosTag = sentence[i + 1][1][0]
+            next2PosTag = sentence[i + 2][1][0]
+
             nextWord = sentence[i + 1][0]
+            next2Word = sentence[i + 2][0]
+
             previousPosTag = sentence[i - 1][1][0]
+            previous2PosTag = sentence[i - 2][1][0]
+
             previousWord = sentence[i - 1][0]
+            previous2Word = sentence[i - 1][0]
 
         word_features.append(previousPosTag)
-        word_features.append(previousWord)
-        word_features.append(nextPosTag)
-        word_features.append(nextWord)
+        word_features.append(previous2PosTag)
 
+        word_features.append(previousWord)
+        word_features.append(previous2Word)
+
+        word_features.append(nextPosTag)
+        word_features.append(next2PosTag)
+
+        word_features.append(nextWord)
+        word_features.append(next2Word)
+        word_features.append(phraseStart)
+        word_features.append(phraseEnd)
+
+        #word_features.append(word[0].isupper())
+        word_features.append(tag_tuple[2])
         features.append(word_features)
     return features
 
@@ -82,7 +119,7 @@ def generate_features(sentence):
 def generate_data(sentences, list_of_features):
     train_data = []
     for i in range(len(sentences)):
-        features = generate_features(train_set[i])
+        features = generate_features(sentences[i])
         train_data = train_data + features
 
     return pd.DataFrame(train_data, columns=list_of_features)
@@ -105,12 +142,29 @@ def convert_data_to_numeric(dataframe, list_of_features):
 
 
 corpus_file_name = 'eng.train.txt'
-list_of_features = ["Word", "PosTag", "NamedEntity", "PreviousPosTag", "PreviousWord", "NextPosTag", "NextWord"]
-list_of_predictors = ["Word", "PosTag", "PreviousPosTag", "PreviousWord", "NextPosTag", "NextWord"]
 
+list_of_predictors = ["Word",
+                      "PosTag",
+                      "PreviousPosTag",
+                      "Previous2PosTag",
+                      "PreviousWord",
+                      "Previous2Word",
+                      "NextPosTag",
+                      "Next2PosTag",
+                      "NextWord",
+                      "Next2Word",
+                      "PhraseStart",
+                      "PhraseEnd"
+                      ]
+
+list_of_features = list_of_predictors + ["NamedEntity"]
+
+
+print list_of_features
 list_of_sentences = generate_sentences(corpus_file_name)
-test_set = list_of_sentences[9000:]
-train_set = list_of_sentences[1:9000]
+
+test_set = list_of_sentences[10000:]
+train_set = list_of_sentences[1:10000]
 
 print ("Test set length : %d" % len(test_set))
 print ("Train set length : %d" % len(train_set))
@@ -121,19 +175,20 @@ train_data_frame.to_csv("NERC_train.csv", index=False)
 numeric_train_frame = convert_data_to_numeric(train_data_frame, list_of_features)
 numeric_train_frame.to_csv("NERC_numeric_train.csv", index=False)
 
-
 test_data_frame = generate_data(test_set, list_of_features)
 test_data_frame.to_csv("NERC_test.csv", index=False)
 
 NamedEntityValues = test_data_frame['NamedEntity']
 
-
+new_test_data_frame = test_data_frame.loc[test_data_frame["NamedEntity"].isin(["I-ORG", "I-PER", "I-LOC"])]
+# print new_test_data_frame[["Word"]]
 
 numeric_test_frame = convert_data_to_numeric(test_data_frame, list_of_features)
+new_numeric_test_frame = convert_data_to_numeric(new_test_data_frame, list_of_features)
 numeric_test_frame.to_csv("NERC_numeric_test.csv", index=False)
 
-#numeric_test_frame = df = numeric_test_frame.drop('NamedEntity', 1)
-#print(numeric_test_frame)
+# numeric_test_frame = df = numeric_test_frame.drop('NamedEntity', 1)
+# print(numeric_test_frame)
 # print(numeric_train_frame)
 
 alg = LogisticRegression(random_state=1)
@@ -143,11 +198,18 @@ print("Traininig")
 
 alg.fit(numeric_train_frame[list_of_predictors], numeric_train_frame["NamedEntity"])
 
-print("Predicting")
+print("Predicting with O")
 predictions = alg.predict(numeric_test_frame[list_of_predictors])
 
+print "Accuracy: ", accuracy_score(numeric_test_frame["NamedEntity"], predictions)
+print "Precision: ", precision_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
+print "Recall: ", recall_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
+print "F1-Score: ", f1_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
 
-print "Accuracy: " , accuracy_score(numeric_test_frame["NamedEntity"], predictions)
-print "Precision: " , precision_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
-print "Recall: " , recall_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
-print "F1-Score: " , f1_score(numeric_test_frame["NamedEntity"], predictions, average='micro')
+print("\n Predicting without O")
+predictions = alg.predict(new_numeric_test_frame[list_of_predictors])
+
+print "Accuracy: ", accuracy_score(new_numeric_test_frame["NamedEntity"], predictions)
+print "Precision: ", precision_score(new_numeric_test_frame["NamedEntity"], predictions, average='micro')
+print "Recall: ", recall_score(new_numeric_test_frame["NamedEntity"], predictions, average='micro')
+print "F1-Score: ", f1_score(new_numeric_test_frame["NamedEntity"], predictions, average='micro')
